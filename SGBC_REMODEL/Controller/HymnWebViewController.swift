dc43e189e85fb1f1ba8d5ad1c5b8c6f9537c6379
temitayo65 <iloritemitayo65@gmail.com>
2022-10-11 13:48:34 +0100

    Renamed WebVC to HymnWebVC

--- SGBC_REMODEL/Controller/HymnWebViewController.swift
+++ SGBC_REMODEL/Controller/HymnWebViewController.swift
@@ -1,5 +1,5 @@
 //
-//  WebViewController.swift
+//  HymnWebViewController.swift
 //  SGBC_REMODEL
 //
 //  Created by ADMIN on 17/09/2022.
@@ -8,22 +8,24 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController, WKNavigationDelegate {
-
+class HymnWebViewController: UIViewController, WKNavigationDelegate {
+    
     var webView: WKWebView!
     var selectedHymn: String!
     
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBarController?.tabBar.isHidden = true 
+        tabBarController?.tabBar.isHidden = true
         webView = WKWebView(frame: CGRect(x: 0.0, y: 0.0, width: 0.1, height: 0.1))
         webView.navigationDelegate = self
         view = webView
-       
+        
         // Do any additional setup after loading the view.
-        let url = Bundle.main.url(forResource: selectedHymn , withExtension: ".pdf")
-        webView.load(URLRequest(url: url!))
+        if selectedHymn != "Hymn 0" { // due to temporary fix
+            let url = Bundle.main.url(forResource: selectedHymn , withExtension: ".pdf")
+            webView.load(URLRequest(url: url!))
+        }
+        
     }
-
 }
